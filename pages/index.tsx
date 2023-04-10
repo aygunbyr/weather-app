723b81a1586a4a29e0eb4463a82decf705fb766e
Aygün Bayır <turkoaygun@gmail.com>
2023-04-10 12:46:56 +0300

    Fix style

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="font-Montserrat w-auto h-screen bg-gradient-to-r from-blue-200 to-gray-200 pt-[10px]">
-      <div className="w-auto h-auto lg:w-[768px] m-auto">
+      <div className="w-auto h-auto md:w-[768px] m-auto">
         <div className="text-2xl p-2 text-center">Hava Durumu</div>
         <div className="bg-red-500 p-2">Şehir Seçimi</div>
         <div className="grid lg:grid-cols-2 gap-5 bg-red-500 p-2">
